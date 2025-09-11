69672be7309b9987fbfd60dee1aea5795494227e
João Matheus Tripoli <joao.tripoli.dev@gmail.com>
2025-09-11 18:13:02 -0300

    Add @tanstack/react-query and @tanstack/eslint-plugin-query dependencies; implement QueryProvider for React Query integration

---
+++ src/providers/query-provider.tsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { type ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+const QueryProvider = ({ children }: Props) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
